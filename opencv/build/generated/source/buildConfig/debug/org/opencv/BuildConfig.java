497b57eb5539b7991d5a1da4905277c527ecb093
Chanwoo jo <whcksdn16@gmail.com>
2023-03-26 18:38:30 +0900

    전체 파일

---
+++ opencv/build/generated/source/buildConfig/debug/org/opencv/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.opencv;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.opencv";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String API_KEY = "[AIzaSyAFbxR-qcDMQf_ZZC2J5ihlPzyUmrll_ks]";
+}
